a1c333efa11c1e45aaf01301baf0dea364984685
9kopb1337 <https://gist.github.com/9kopb1337>
2022-09-17 22:37:32 +0300

    initial commit

---
+++ FirstSemestrFinish/task.js
@@ -0,0 +1,11 @@
+
+function getThreeSymbolString(array){
+    let newArray = [];
+    let i = 0;
+    for (i = 0; i < array.length; i++) {
+        if (array[i].length < 4) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
